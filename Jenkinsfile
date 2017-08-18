7452fef1190162f76c288916def3953a32c70af8
jason <lvzhijun@synjones.com>
2017-08-18 21:56:46 +0800

    add test and clean up

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,4 +9,10 @@ node {
     stage('docker') {
 	sh 'bash docker_build.sh'
     }
+    stage('test') {
+	sh 'curl http://localhost:8888'
+    }
+    stage('clean') {
+	sh 'sudo docker rm -f helloworld'
+    }
 }
--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,4 @@
 # build
 sudo docker build . -t 4admin2root/cloud-simple-helloworld:latest
 # run docker
-sudo docker run -d -p 8888:8080 4admin2root/cloud-simple-helloworld:latest
+sudo docker run -d --rm --name helloworld -p 8888:8080 4admin2root/cloud-simple-helloworld:latest
