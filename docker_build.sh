04d5befeb42ef64b4e7c3a8bb5b31809b21d60bc
jason <lvzhijun@synjones.com>
2017-08-18 15:51:25 +0800

    spilit stage

---
+++ docker_build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+cd myhomework/
+# build
+sudo docker build . -t 4admin2root/cloud-simple-helloword:latest
+# run docker
+sudo docker run -d -p 8888:8080 4admin2root/cloud-simple-helloword:latest
