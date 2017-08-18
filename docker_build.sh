a5231b78f9e3a5b46de569ba005093bcff4a33bb
jason <lvzhijun@synjones.com>
2017-08-18 22:25:34 +0800

    add docker rm helloworld

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,7 @@
 sudo docker build . -t 4admin2root/cloud-simple-helloworld:latest
 # run docker
 echo 'run docker image'
+set +e
+sudo docker rm -f helloworld
+set -e
 sudo docker run -d --rm --name helloworld -p 8888:8080 4admin2root/cloud-simple-helloworld:latest
